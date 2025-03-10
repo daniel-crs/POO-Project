7c2696c631b96c21ef077c65fd74310dc906cc0a
Gustavo <decastrogustavo19@proton.me>
2025-03-10 14:33:32 -0300

    draft of the electronics class.

--- StockDosBons/src/entities/Electronics.java
+++ StockDosBons/src/entities/Electronics.java
@@ -1,6 +1,40 @@
 package entities;
 
 public class Electronics {
-    
+    private String names;
+    private double price;
+    private double energyCost;
+    private String fabricator;
 
+    public String getNames() {
+        return names;
+    }
+
+    public void setNames(String names){
+        this.names=names;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price){
+        this.price=price;
+    }
+
+    public double getEnergyCost() {
+        return energyCost;
+    }
+
+    public void setEnergyCost(double energyCost) {
+        this.energyCost = energyCost;
+    }
+
+    public String getFabricator() {
+        return fabricator;
+    }
+
+    public void setFabricator(String fabricator) {
+        this.fabricator = fabricator;
+    }
 }
