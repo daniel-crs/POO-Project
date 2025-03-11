610c2eae04500cca2c33fa6370ac2bf1b5573cf2
Matheus França <matheusfrancag2003@gmail.com>
2025-03-11 20:46:25 -0300

    Desenvolvimento dos getters e setters da entidade Hortifruti

--- StockDosBons/src/application/App.java
+++ StockDosBons/src/application/App.java
@@ -1,7 +1,23 @@
 package application;
 
+import java.util.Scanner;
+import entities.Hortifruti;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite o nome da fruta: ");
+        String name = input.next();
+
+        System.out.print("Digite o preço por kg: ");
+        double pricePerKilo = input.nextDouble();
+
+        System.out.print("Digite a quantidade em stock: ");
+        int quantity = input.nextInt();
+
+        Hortifruti fruit = new Hortifruti(name, pricePerKilo, quantity);
+
+        System.out.println("Quantidade: " + fruit.getQuantity());
     }
 }
--- StockDosBons/src/entities/Hortifruti.java
+++ StockDosBons/src/entities/Hortifruti.java
@@ -1,5 +1,37 @@
 package entities;
 
 public class Hortifruti {
+    private String name;
+    private double pricePerKilo;
+    private int quantity;
 
+    public Hortifruti(String name, double pricePerKilo, int quantity) {
+        this.name = name;
+        this.pricePerKilo = pricePerKilo;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPricePerKilo() {
+        return pricePerKilo;
+    }
+
+    public void setPricePerKilo(double pricePerKilo) {
+        this.pricePerKilo = pricePerKilo;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
