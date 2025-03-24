9169762bbaff9081c52d9ce7286ac03c491b412f
Daniel <daniel.reis1911@gmail.com>
2025-03-24 09:56:25 -0300

    Atualizando a App.

--- StockDosBons/src/application/App.java
+++ StockDosBons/src/application/App.java
@@ -8,6 +8,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
 
+        int option, flag = 0;
+
         System.out.print("Digite o nome da fruta: ");
         String name = input.next();
 
@@ -19,9 +21,6 @@ public class App {
 
         Hortifruti fruit = new Hortifruti(name, price, quantity);
 
-        System.out.print(fruit.toString());
-
-        
         System.out.print("Digite o nome do eletrônico: ");
         String nameE = input.next();
 
@@ -33,7 +32,7 @@ public class App {
 
         System.out.print("Digite o fabricante: ");
         String fabricator = input.next();
-        
+
         System.out.print("Digite o modelo: ");
         String model = input.next();
 
@@ -42,6 +41,45 @@ public class App {
 
         Electronics electronic = new Electronics(nameE, mark, priceE, fabricator, model, yearLaunch);
 
-        System.out.print(electronic.toString());
+        do {
+            info_options();
+
+            System.out.print("\nChoose a option: ");
+            option = input.nextInt();
+
+            switch (option) {
+
+                case 1:
+                    System.out.print(fruit.toString());
+                    break;
+
+                case 2:
+                    System.out.print(electronic.toString());
+                    break;
+
+                case 3:
+                    System.out.print("Eletronics vai aqui.");
+                    break;
+
+                case 0:
+                    flag = 1;
+                    break;
+
+            }
+        } while (flag != 1);
+
+        System.out.println("Finalizando!!!");
+
+        input.close();
+    }
+
+    public static void info_options() {
+
+        System.out.print("\n     Stock dos Bons");
+        System.out.print("\n1 - Hortifruti Listing");
+        System.out.print("\n2 - Furniture Listing");
+        System.out.print("\n3 - Electronics Listing");
+        System.out.print("\n0 - exit");
+
     }
 }
